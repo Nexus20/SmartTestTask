8e8026d35c1ba64370a79c6a129724431639120f
Yevhen Chubarov <40770350+Nexus20@users.noreply.github.com>
2023-02-27 22:39:58 +0200

    Added migrations

---
+++ SmartTestTask.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SmartTestTask.Infrastructure;
+
+public static class InfrastructureServicesRegistration
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseLazyLoadingProxies().UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        return services;
+    }
+}
