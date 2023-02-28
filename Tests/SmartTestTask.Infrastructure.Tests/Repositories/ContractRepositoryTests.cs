f2a5baf334f060b352bb72ade514677d184fd25f
Yevhen Chubarov <40770350+Nexus20@users.noreply.github.com>
2023-02-28 21:35:53 +0200

    Added contract repository tests

---
+++ Tests/SmartTestTask.Infrastructure.Tests/Repositories/ContractRepositoryTests.cs
@@ -0,0 +1,106 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using SmartTestTask.Application.Interfaces.Repositories;
+using SmartTestTask.Domain.Entities;
+using SmartTestTask.Infrastructure.Repositories;
+
+namespace SmartTestTask.Infrastructure.Tests.Repositories;
+
+[TestFixture]
+public class ContractRepositoryTests
+{
+    private ApplicationDbContext _dbContext = null!;
+    private IContractRepository _contractRepository = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _dbContext = new ApplicationDbContext(UnitTestsHelper.GetUnitTestDbOptions());
+        _contractRepository = new ContractRepository(_dbContext);
+    }
+
+    [Test]
+    public async Task CreateContractAsync_Always_CreatesContract()
+    {
+        // Arrange
+        var expectedResult = await _dbContext.Contracts.CountAsync() + 1;
+
+        // Act
+        await _contractRepository.CreateContractAsync(new Contract()
+        {
+            IndustrialPremiseId = "1",
+            TechnicalEquipmentTypeId = "2",
+            Count = 10
+        });
+        var actualResult = await _dbContext.Contracts.CountAsync();
+
+        // Assert
+        actualResult.Should().Be(expectedResult);
+    }
+
+    [Test]
+    public async Task GetAsync_WhenDataIsPresent_ReturnsContractsList()
+    {
+        // Arrange
+        var expectedResult = await _dbContext.Contracts
+            .Include(x => x.IndustrialPremise)
+            .Include(x => x.TechnicalEquipmentType)
+            .AsNoTracking()
+            .ToListAsync();;
+
+        // Act
+        var actualResult = await _contractRepository.GetAsync();;
+
+        // Assert
+        actualResult.Should().BeEquivalentTo(expectedResult, o => o.IgnoringCyclicReferences());
+    }
+    
+    [Test]
+    public async Task GetAsync_WhenThereIsNoData_ReturnsEmptyList()
+    {
+        // Arrange
+        var contracts = await _dbContext.Contracts.ToListAsync();
+        _dbContext.Contracts.RemoveRange(contracts);
+        await _dbContext.SaveChangesAsync();
+
+        // Act
+        var actualResult = await _contractRepository.GetAsync();;
+
+        // Assert
+        actualResult.Should().BeEmpty();
+    }
+
+    [Test]
+    public async Task GetSingleByExpressionAsync_WhenContractIsPresent_ReturnsContractByExpression()
+    {
+        // Arrange
+        var expectedResult = await _dbContext.Contracts
+            .AsNoTracking()
+            .SingleOrDefaultAsync(x => x.Id == "1");
+
+        // Act
+        var actualResult = await _contractRepository.GetSingleByExpressionAsync(x => x.Id == "1");;
+
+        // Assert
+        actualResult.Should().BeEquivalentTo(expectedResult);
+    }
+    
+    [Test]
+    public async Task GetSingleByExpressionAsync_WhenNoContractFound_ReturnsNull()
+    {
+        // Arrange
+
+        // Act
+        var actualResult = await _contractRepository.GetSingleByExpressionAsync(x => x.Id == "1234");;
+
+        // Assert
+        actualResult.Should().BeNull();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _dbContext.Dispose();
+    }
+}
---
+++ Tests/SmartTestTask.Infrastructure.Tests/UnitTestsHelper.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using SmartTestTask.Domain.Entities;
+
+namespace SmartTestTask.Infrastructure.Tests;
+
+internal static class UnitTestsHelper
+{
+    public static DbContextOptions<ApplicationDbContext> GetUnitTestDbOptions()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+            .Options;
+
+        using var context = new ApplicationDbContext(options);
+        SeedData(context);
+
+        return options;
+    }
+
+    private static void SeedData(ApplicationDbContext context)
+    {
+        // Seed the needed data here into inMemory Db context
+
+        if (!context.IndustrialPremises.Any())
+        {
+            context.IndustrialPremises.AddRange(
+                new IndustrialPremise
+                {
+                    Id = "1",
+                    Area = 100,
+                    Code = "TEP-1",
+                    Name = "Test premise 1"
+                },
+                new IndustrialPremise
+                {
+                    Id = "2",
+                    Area = 50,
+                    Code = "TEP-2",
+                    Name = "Test premise 2"
+                },
+                new IndustrialPremise
+                {
+                    Id = "3",
+                    Area = 96,
+                    Code = "TEP-3",
+                    Name = "Test premise 3"
+                }
+                );
+        }
+
+        if (!context.TechnicalEquipmentTypes.Any())
+        {
+            context.TechnicalEquipmentTypes.AddRange(new TechnicalEquipmentType
+                {
+                    Id = "1",
+                    Area = 10,
+                    Code = "TTET-1",
+                    Name = "Test equipment type 1"
+                },
+                new TechnicalEquipmentType
+                {
+                    Id = "2",
+                    Area = 12,
+                    Code = "TTET-2",
+                    Name = "Test equipment type 2"
+                },
+                new TechnicalEquipmentType
+                {
+                    Id = "3",
+                    Area = 8,
+                    Code = "TTET-3",
+                    Name = "Test equipment type 3"
+                });
+        }
+
+        if (!context.Contracts.Any())
+        {
+            context.Contracts.AddRange(new Contract
+                {
+                    Id = "1",
+                    Count = 10,
+                    IndustrialPremiseId = "1",
+                    TechnicalEquipmentTypeId = "3",
+                },
+                new Contract
+                {
+                    Id = "2",
+                    Count = 2,
+                    IndustrialPremiseId = "1",
+                    TechnicalEquipmentTypeId = "2",
+                },
+                new Contract
+                {
+                    Id = "3",
+                    Count = 5,
+                    IndustrialPremiseId = "2",
+                    TechnicalEquipmentTypeId = "1",
+                },
+                new Contract
+                {
+                    Id = "4",
+                    Count = 8,
+                    IndustrialPremiseId = "3",
+                    TechnicalEquipmentTypeId = "2",
+                },
+                new Contract
+                {
+                    Id = "5",
+                    Count = 1,
+                    IndustrialPremiseId = "3",
+                    TechnicalEquipmentTypeId = "1",
+                });
+        }
+
+        context.SaveChanges();
+    }
+}
